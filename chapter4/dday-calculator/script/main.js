78f42e7980d781ed85902371ccd218dfa5541324
eunje0ng <100328746+eunje0ng@users.noreply.github.com>
2022-03-24 12:27:54 +0900

    디데이계산기 실습0324

--- chapter4/dday-calculator/script/main.js
+++ chapter4/dday-calculator/script/main.js
@@ -3,6 +3,7 @@ var startBTN=document.querySelector("#calc");
 startBTN.addEventListener("click", changeScene);
 
 function changeScene(e){
+    // console.log(e);
     var articleTAGS = document.querySelectorAll("article");
     articleTAGS[0].classList.add("d-none");
     articleTAGS[1].classList.remove("d-none");
@@ -10,24 +11,32 @@ function changeScene(e){
 
 var ddayFORM = document.querySelector("#ddayForm");
 
-var resetBTN = ddayFORM.querySelector("#dday_reset");
+var calcBTN = document.querySelector("#ddayCalc");
+
+var resetBTN = ddayFORM.querySelector("#ddayReset");
 
 ddayFORM.addEventListener("submit", ddayCalc);
 
-function ddayCalc(){
+function ddayCalc(e){
     e.preventDefault(); //화면새로고침방지
-    var userYear = document.querySelector("#userYear").Value;
-    var userMonth = document.querySelector("#userMonth").Value;
-    var userDate = document.querySelector("#userDate").Value;
+    
+    var userYEAR = document.querySelector("#userYear").value;
+    var userMONTH = document.querySelector("#userMonth").value;
+    var userDATE = document.querySelector("#userDate").value;
 
     //디데이 계산 출력하기
     var today = new Date();
-    var nowMonth = today.getMonth();
-    var theDate = new Date(userYear,userMonth-1,userDate);
+    
+    var theDate = new Date(userYEAR, userMONTH-1, userDATE);
 
     var diffDate = theDate.getTime() - today.getTime();
 
     var result = Math.ceil(diffDate / (1000*60*60*24));
 
-    alert("D-day :"+result);
+    // alert("D-day :"+result); -팝업창으로 만들자
+    var ddayPOP = document.querySelector("#result_pop");
+    var ddayTEXT = ddayPOP.querySelector("#d_day");
+    ddayTEXT.innerHTML = `<strong>${result}</strong> 일`;
+    //팝업.클래스목록.제거("d-none");
+    ddayPOP.classList.remove("d-none");
 }
