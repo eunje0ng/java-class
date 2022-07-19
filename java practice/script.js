6be5de7bd3eaf4db8b6076040d85ad4984cdd9da
eunje0ng <100328746+eunje0ng@users.noreply.github.com>
2022-07-19 09:54:39 +0900

    Add files via upload

---
+++ java practice/script.js
@@ -0,0 +1,9 @@
+// alert('허리아퍼 ㅜㅜ')
+
+document.write("<h1>환영합니다.</h1>")
+document.write("<p>띠용</p>")
+
+/*자주 사용하는 메소드(=명령어)
+1. document.write() : body영역에 내용 출력(태그도 출력)
+2. alret() : 화면에 경고창, 알림창을 띄움
+3. console.log() : 검사 창에서 콘솔에 출력하는 명령 */
